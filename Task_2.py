61d8a3844996b658cdcbc2986df170ace58f2b40
Joshua White <140011936+Vircho@users.noreply.github.com>
2025-04-16 11:21:44 -0600

    Bugfixes and added sheet functionality
    Fixed several bugs
    - Incorrect link on requests.get when getting 2024 papers
    - Incorrect values passed to parameters of fill_contrib_dict() when filling each contribution dictionary
    
    - Added function to try/except for KeyError
    
    -Added code that fills a sheet with the top contributors

--- Task_2.py
+++ Task_2.py
@@ -4,6 +4,7 @@ import bs4  #allows BeautifulSoup useful methods
 import openpyxl #allows excel sheet modification
 import sys  #allows sys.maxsize
 from openpyxl.utils import get_column_letter
+
 #fill_contrib_dict - takes a list of elements representing author names in the site and a dictionary to fill
 #then loops through all the text attributes (the author name) of each element. If that author is not in the
 #dictionary, add them and set contributions to 1, if they are, just increment their contributions
@@ -64,6 +65,15 @@ def get_largest_value_dict(dictionary, largest):
             this_largest = value
     return this_largest
 
+#try_for_keyerror - does a try/except for KeyError, returns status
+def try_for_keyerror(dictionary, key):
+
+    try:
+        val = dictionary[key]
+    except KeyError:
+        return 1
+    else:
+        return 0
 
 #Variables
 contrib22 = dict()  #dictionaries for {name, papers} for each year
@@ -74,7 +84,7 @@ contrib_tot = dict()
 #Get the site of each year as a Response Object
 res22 = requests.get("https://openaccess.thecvf.com/CVPR2022?day=all")
 res23 = requests.get("https://openaccess.thecvf.com/CVPR2023?day=all")
-res24 = requests.get("https://openaccess.thecvf.com/CVPR2020?day=all")
+res24 = requests.get("https://openaccess.thecvf.com/CVPR2024?day=all")
 
 #Create a soup for each site
 soup22 = bs4.BeautifulSoup(res22.text, "html.parser")
@@ -88,8 +98,8 @@ papers24 = soup24.find_all("form", class_='authsearch')
 
 #fill the contribution dictionaries with the author name and how many papers they worked on
 fill_contrib_dict(papers22, contrib22)
+fill_contrib_dict(papers23, contrib23)
 fill_contrib_dict(papers24, contrib24)
-fill_contrib_dict(papers22, contrib22)
 
 #Merge the dictionaries to find total contributions of each author
 contrib_tot = merge_dicts(contrib_tot, contrib22, contrib23)
@@ -121,4 +131,32 @@ print(largest_contrib)
 wb = openpyxl.load_workbook('top_contributors.xlsx')
 sheet = wb['Sheet1']
 
-# TODO: Make the table in the excel sheet
+#Fill out the sheet row by row
+#This is a pretty brute-force way to do it, but it worked
+for this_row in range(1, 5 + 1):   #rows
+
+    for this_column in range(1, 4 + 1):   #columns
+
+        if this_row == 1 and this_column != 1:
+            sheet.cell(row = this_row, column = this_column).value = largest_contrib[this_column - 2]
+
+        elif this_column == 1 and this_row != 1:
+            if this_row != 5:
+                sheet.cell(row = this_row, column = this_column).value = str(2022 + (this_row - 2))
+            else:
+                sheet.cell(row=this_row, column=this_column).value = 'Total'
+
+        elif this_row != 1 and this_column != 1:
+            if this_row == 2 and not try_for_keyerror(contrib22, largest_contrib[this_column - 2]):
+                sheet.cell(row = this_row, column = this_column).value = contrib22[largest_contrib[this_column - 2]]
+
+            elif this_row == 3 and not try_for_keyerror(contrib23, largest_contrib[this_column - 2]):
+                sheet.cell(row=this_row, column=this_column).value = contrib23[largest_contrib[this_column - 2]]
+
+            elif this_row == 4 and not try_for_keyerror(contrib23, largest_contrib[this_column - 2]):
+                sheet.cell(row=this_row, column=this_column).value = contrib24[largest_contrib[this_column - 2]]
+
+            elif this_row == 5:
+                sheet.cell(row = this_row, column = this_column).value = largest_amounts[this_column - 2]
+
+wb.save('top_contributors.xlsx')
